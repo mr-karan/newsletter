053240fb17abf23ad72e7f67f98b9393e082db45
mr-karan <karansharma1295@gmail.com>
2019-11-12 09:30:43 +0530

    feat: reduce css file size

---
+++ frontend/postcss.config.js
@@ -0,0 +1,17 @@
+const tailwindcss = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+    plugins: [
+        tailwindcss('./tailwind.js'),
+        cssnano({
+            preset: 'default',
+        }),
+        purgecss({
+            content: ['./index.html']
+        }),
+        autoprefixer
+    ]
+}
